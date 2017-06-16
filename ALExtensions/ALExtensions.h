2f344817da9dbfa25c7ac2d7f3a2b0cab216aaf5
alexlivenson <alexlivenson@gmail.com>
2017-06-15 22:15:07 -0700

    Initial Commit

---
+++ ALExtensions/ALExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ALExtensions.h
+//  ALExtensions
+//
+//  Created by Alex Livenson on 6/15/17.
+//  Copyright © 2017 Alex Livenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALExtensions.
+FOUNDATION_EXPORT double ALExtensionsVersionNumber;
+
+//! Project version string for ALExtensions.
+FOUNDATION_EXPORT const unsigned char ALExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALExtensions/PublicHeader.h>
+
+
