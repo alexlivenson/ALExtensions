70256a6e66e3d950d77468ad96b238553fb4464a
alexlivenson <alexlivenson@gmail.com>
2019-04-19 10:35:46 -0700

    Alex L - Updated for swift 5

--- ALExtensions/UIViewController+Extensions.swift
+++ ALExtensions/UIViewController+Extensions.swift
@@ -14,22 +14,22 @@ public extension UIViewController {
     }
     
     func addChild(viewCtrl: UIViewController, to subView: UIView) {
-        addChildViewController(viewCtrl)
-        viewCtrl.didMove(toParentViewController: self)
+        addChild(viewCtrl)
+        viewCtrl.didMove(toParent: self)
         subView.addSubview(viewCtrl.view)
     }
     
     func addAndFillChild(viewCtrl: UIViewController, to subView: UIView) {
-        addChildViewController(viewCtrl)
-        viewCtrl.didMove(toParentViewController: self)
+        addChild(viewCtrl)
+        viewCtrl.didMove(toParent: self)
         subView.addSubview(viewCtrl.view)
         viewCtrl.view.translatesAutoresizingMaskIntoConstraints = false
         viewCtrl.view.fillToParent()
     }
     
     func removeChild(viewCtrl: UIViewController) {
-        viewCtrl.willMove(toParentViewController: nil)
+        viewCtrl.willMove(toParent: nil)
         viewCtrl.view.removeFromSuperview()
-        viewCtrl.removeFromParentViewController()
+        viewCtrl.removeFromParent()
     }
 }
